7e4e76f0b6d760932855abc40c3cab5c42ae75ac
Kaylie Alyssa <eleckctra@Pharis.airstreamcomm.net>
2016-05-29 01:47:38 -0500

    Adds test.java. This file will be replaced by team project. For now it lets the team get used to working on a file under version control.

---
+++ LiarsDice/test.java
@@ -0,0 +1,15 @@
+
+public class test {
+
+    /**
+     * Test is for team to play around with so everyone can get 
+     * used to working with version control. Feel free to add, modify,
+     * and delete code.
+     * 
+     * @param args
+     */
+	public static void main(String[] args) {
+		System.out.println("OO7 reporting for duty");
+	}
+
+}
