43f3081b31fb2733c580158813d527b4f831aa3a
Jacob P. Williams <jacobpwilliams@hotmail.com>
2016-06-20 23:23:24 -0400

    added comments

--- LiarsDice/Die.java
+++ LiarsDice/Die.java
@@ -1,17 +1,38 @@
 import java.util.*;
-public class Die {
-	private int faceValue = 0;
+/*
+ * @name : Die
+ * @author : Cameron Walworth, Abraham Assad
+ * @decr :
+ */public class Die {
+    private int faceValue = 0;
 
-	public Die(){   //Is the constructor for the class
-	    
-	}
-	public int getFaceValue(){   //Produces the face value for the die
-		return faceValue;
-	}
-	public int generateSpots(){   //Generates at random the spots and values
-		Random r = new Random();
-		faceValue = r.nextInt(6)+1;
-		return faceValue;
-	}
+    /*
+     * @name : Die
+     * @decr : Is the constructor for the class
+     * @param : void
+     */
+    public Die(){  
+        
+    }
+    
+    /*
+     * @name : getFaceValue
+     * @decr : Produces the face value for the die
+     * @param : int
+     */
+    public int getFaceValue(){   
+        return faceValue;
+    }
+    
+    /*
+     * @name : generateSpots
+     * @decr : Generates at random the spots and values
+     * @param : int
+     */
+    public int generateSpots(){   
+        Random r = new Random();
+        faceValue = r.nextInt(6)+1;
+        return faceValue;
+    }
 }
 
